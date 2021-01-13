6aa614e2aa0179c4e3ccc34e9c31e3ba781b0b1f
rottenkernel <mariaratazzz@gmail.com>
2021-01-13 21:56:47 +1000

    relize v1

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main', 
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes);
+
+
+async function start() {
+    try {
+      await mongoose.connect(process.env.TOKEN, {
+          useNewUrlParser: true,
+          useFindAndModify: false
+        }) 
+        app.listen(PORT,() => {
+          console.log('Server has been started...')
+        })
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+start();
